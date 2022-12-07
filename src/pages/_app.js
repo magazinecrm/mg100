fd813188c757905afc5f08fd6e76860f6f238de8
magazinecrm <paul@magazinecrm.com>
2022-12-08 00:21:55 +0530

    Godisgreat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,9 @@ import "@/vendors/lums-icon/style.css";
 import "@/fonts/spartan-mb/stylesheet.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { GA_TRACKING_ID } from '/gtab.js'
 
 
 import React, { useEffect } from "react";
@@ -18,6 +21,18 @@ import "@/styles/style.css";
 import "@/styles/responsive.css";
 
 const MyApp = ({ Component, pageProps }) => {
+	 const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+	  return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (    
 
  <ContextProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,6 +16,9 @@ export default function Document() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+			gtag('config','${GA_TRACKING_ID}', {
+				page_path: window.location.pathname,
+			});
 
           `,
           }}
